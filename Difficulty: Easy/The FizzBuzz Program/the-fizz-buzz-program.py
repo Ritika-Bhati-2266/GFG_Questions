de4161407b6c5849e7e79a2b3c72020be2de7b56
Ritika-Bhati-2266 <ritika452044@gmail.com>
2026-06-07 10:33:12 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/The FizzBuzz Program/the-fizz-buzz-program.py
+++ Difficulty: Easy/The FizzBuzz Program/the-fizz-buzz-program.py
@@ -1,11 +1,11 @@
 class Solution:
-    def fizzBuzz(self,number):
-        # code here.
-        if number%3==0 and number %5==0:
+    def fizzBuzz(self, number):
+        # code here
+        if number % 3 == 0 and number % 5 ==0:
             print("FizzBuzz")
-        elif number%3 == 0:
-            print("Fizz")
-        elif number%5 == 0:
+        elif number % 5 == 0:
             print("Buzz")
+        elif number % 3 ==0:
+            print("Fizz")
         else:
             print(number)
